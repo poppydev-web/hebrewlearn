5167833bd52ef3b116f522e0c35deef4c57ac0e6
poppydev <pinkypoppy0806@outlook.com>
2024-10-10 01:54:00 +0700

    changed port number

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const bodyParser = require("body-parser");
 const lesson = require("./lessonData");
 
 const app = express();
-const port = process.env.PORT || 5001;
+const port = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors());

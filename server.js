b61c0eca5abaa13c6f0cbd59654413194c08f790
poppydev <pinkypoppy0806@outlook.com>
2024-10-09 23:37:57 +0700

    fixed require file name

--- server.js
+++ server.js
@@ -3,7 +3,7 @@
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
-const lesson = require("./LessonData");
+const lesson = require("./lessonData");
 
 const app = express();
 const port = process.env.PORT || 5000;
